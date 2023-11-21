0f5d81cc16815b1193cb2e76710a16907f979987
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-11-21 19:24:18 +0100

    Add switch to nominal mode command
    Add the "switch to nominal mode" (ICD page 45).
    
    Issue: https://github.com/davincisatellite/msp-flightsoftware/issues/5

--- src/EPS.h
+++ src/EPS.h
@@ -51,6 +51,7 @@ public:
     static standard_reply system_reset(DWire &wire, uint8_t i2c_address);
     static standard_reply cancel_operation(DWire &wire, uint8_t i2c_address);
     static standard_reply watchdog(DWire &wire, uint8_t i2c_address);
+    static standard_reply switch_nominal_mode(DWire &wire, uint8_t i2c_address);
 };
 
 #endif //EPS_CONVERSION_EPS_H
