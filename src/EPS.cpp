e0abfc4f034c037477ebd772db0a48aeef6bc992
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-11-21 18:42:48 +0100

    Add output bus group state command
    Add the "output bus group state command" (ICD page 42).
    
    Issue: https://github.com/davincisatellite/msp-flightsoftware/issues/5

--- src/EPS.cpp
+++ src/EPS.cpp
@@ -164,3 +164,42 @@ EPS::standard_reply EPS::watchdog(DWire &wire, uint8_t i2c_address) {
     return reply;
 }
 
+EPS::standard_reply EPS::output_bus_group_state(DWire &wire, uint8_t i2c_address, uint16_t bitflag) {
+    standard_reply reply;
+
+
+    /* Write command to EPS */
+    wire.beginTransmission(i2c_address);
+    wire.write(0x00);
+    wire.write(0x06);
+    wire.write(0x14);
+    wire.write(0x00);
+
+    uint8_t bytes[2];
+    bytes[0] = (uint8_t)(x >> 8);   // most significant byte
+    bytes[1] = (uint8_t)(x & 0xFF); // least significant byte
+
+    wire.write(bytes[1])
+    wire.write(bytes[0])
+
+    // delay
+    delay_ms(25);
+
+    // request 5 bytes of data (i.e) the length of the response
+    uint8_t response = wire.requestFrom(i2c_address, 5);
+
+    // if response if 5 bytes long populate reply struct else mark error
+    if (response == 5) {
+        reply.stid = wire.read();
+        reply.ivid = wire.read();
+        reply.rc = wire.read();
+        reply.bid = wire.read();
+        reply.stat = wire.read();
+        reply.error = false;
+    } else {
+        reply.error = true;
+    }
+
+    return reply;
+
+}
