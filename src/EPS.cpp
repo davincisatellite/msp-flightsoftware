e2aedd4ba6878a2ee2a39cab736110fd4e736c8c
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-11-21 16:56:19 +0100

    Add get system status command
    Add "get system status" command (ICD page 47).
    
    Issue: https://github.com/davincisatellite/msp-flightsoftware/issues/5

--- src/EPS.cpp
+++ src/EPS.cpp
@@ -164,3 +164,67 @@ EPS::standard_reply EPS::watchdog(DWire &wire, uint8_t i2c_address) {
     return reply;
 }
 
+EPS::system_status_reply EPS::get_system_status(DWire &wire, uint8_t i2c_address) {
+    standard_reply reply;
+
+    /* Write command to EPS */
+    wire.beginTransmission(i2c_address);
+    wire.write(0x00);
+    wire.write(0x06);
+    wire.write(0x40);
+    wire.write(0x00);
+
+    // delay
+    delay_ms(25);
+
+    // request 5 bytes of data (i.e) the length of the response
+    uint8_t response = wire.requestFrom(i2c_address, 36);
+
+    // if response if 5 bytes long populate reply struct else mark error
+    if (response == 36) {
+        uint8_t* data = nullptr;
+
+        reply.stid = wire.read();
+        reply.ivid = wire.read();
+        reply.rc = wire.read();
+        reply.bid = wire.read();
+        reply.stat = wire.read();
+        reply.mode = wire.read();
+        reply.conf = wire.read();
+        reply.reset_cause = wire.read();
+
+        data = wire.read(4);
+        memcpy(&reply.uptime, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.system_error, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.rc_cnt_pwron, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.rc_cnt_wdg, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.rc_cnt_cmd, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.rc_cnt_mcu, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.rc_cnt_emlopo, data, sizeof data);
+
+        data = wire.read(2);
+        memcpy(&reply.prevcmd_elapsed, data, sizeof data);
+
+        data = wire.read(10);
+        memcpy(&reply.internal_use, data, sizeof data);
+
+        reply.error = false;
+    } else {
+        reply.error = true;
+    }
+
+    return reply;
+
+}
