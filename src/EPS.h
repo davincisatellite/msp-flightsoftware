a94d5ba96e7cfc393a0ce805af707b087e3c9a74
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-11-21 17:10:45 +0100

    Add get system status command
    Add "get system status" command (ICD page 47).
    
    Issue: https://github.com/davincisatellite/msp-flightsoftware/issues/5

--- src/EPS.h
+++ src/EPS.h
@@ -51,6 +51,7 @@ public:
     static standard_reply system_reset(DWire &wire, uint8_t i2c_address);
     static standard_reply cancel_operation(DWire &wire, uint8_t i2c_address);
     static standard_reply watchdog(DWire &wire, uint8_t i2c_address);
+    static standard_reply get_system_status(DWire &wire, uint8_t i2c_address);
 };
 
 #endif //EPS_CONVERSION_EPS_H
