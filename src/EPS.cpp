6b33ee10ca2408c9de717b7f04ed84e3e456b42c
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-12-05 14:59:25 +0100

    Add Get PCU Housekeeping Data (ENG) command
    Add the "Get PCU Housekeeping Data (ENG)" (ICD page 66).
    
    Issue: https://github.com/davincisatellite/msp-flightsoftware/issues/5

--- src/EPS.cpp
+++ src/EPS.cpp
@@ -164,3 +164,50 @@ EPS::standard_reply EPS::watchdog(DWire &wire, uint8_t i2c_address) {
     return reply;
 }
 
+EPS::pcu_housekeeping_data_reply EPS::get_pcu_housekeeping_data_eng(DWire &wire, uint8_t i2c_address) {
+    pcu_housekeeping_data_reply reply;
+
+    /* Write command to EPS */
+    wire.beginTransmission(i2c_address);
+    wire.write(0x00);
+    wire.write(0x06);
+    wire.write(0x72);
+    wire.write(0x00);
+
+    // delay
+    delay_ms(25);
+
+    // request 5 bytes of data (i.e) the length of the response
+    uint8_t response = wire.requestFrom(i2c_address, 5);
+
+    // if response if 5 bytes long populate reply struct else mark error
+    if (response == 5) {
+        reply.stid = wire.read();
+        reply.ivid = wire.read();
+        reply.rc = wire.read();
+        reply.bid = wire.read();
+        reply.stat = wire.read();
+
+        // read (reserved) and discard
+        wire.read();
+
+        reply.volt_brdsup = wire.read() + (wire.read() << 1);
+        reply.temp = wire.read() + (wire.read() << 1);
+
+        for (int i = 0; i < 6; ++i) {
+            reply.vip_output[i] = wire.read();
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 14; ++j) {
+                reply.cc[i][j] = wire.read();
+            }
+        }
+
+        reply.error = false;
+    } else {
+        reply.error = true;
+    }
+
+    return reply;
+}
--- src/EPS.h
+++ src/EPS.h
@@ -46,11 +46,27 @@ public:
         uint8_t error;
     };
 
+    struct pcu_housekeeping_data_reply {
+        uint8_t stid;
+        uint8_t ivid;
+        uint8_t rc;
+        uint8_t bid;
+        uint8_t stat;
+
+        uint16_t volt_brdsup;
+        uint16_t temp;
+        uint8_t vip_output[6];
+        uint8_t cc[4][14];      // Response CC_1 stored in cc[1]. Bytes are kept in the order that they are read
+
+        uint8_t error;
+    };
+
     static standard_reply reset_watchdog(DWire &wire, uint8_t i2c_address);
     static standard_reply no_operation(DWire &wire, uint8_t i2c_address);
     static standard_reply system_reset(DWire &wire, uint8_t i2c_address);
     static standard_reply cancel_operation(DWire &wire, uint8_t i2c_address);
     static standard_reply watchdog(DWire &wire, uint8_t i2c_address);
+    static pcu_housekeeping_data_reply get_pcu_housekeeping_data_eng(DWire &wire, uint8_t i2c_address);
 };
 
 #endif //EPS_CONVERSION_EPS_H
