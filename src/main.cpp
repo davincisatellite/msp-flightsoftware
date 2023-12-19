7468a78133f0b18d79f1c4959a8d827a22e2bcec
rusudanmachavariani <rusudan@elnino.tech>
2023-12-19 18:42:26 +0100

    Working transmit c++

--- src/main.cpp
+++ src/main.cpp
@@ -28,40 +28,100 @@
  *   Lastly, this is a very empty project. We are not sure how much memory will be free in the end for buffering
  *   the image, or how much processing power we can reserve for this.
  */
-
-//#include "msp.h"
-//#include "Console.h"
+#include "msp.h"
+#include "Console.h"
 #include "DelfiPQcore.h"
 #include "delay.h"
 
-int main(void) {
+/* Slave Address for I2C Slave. */
+#define SLAVE_ADDRESS 0x48
+
+/* Length of the buffer. Image chunk size must be smaller than this! */
+#define BUFFER_LENGTH 1024
+
+/* Buffer to store data in. */
+static uint8_t TXData[BUFFER_LENGTH] = { 0 };
+
+/* Length of the received data. */
+static uint16_t TXLength = 0;
+
+/* Number of image bytes received. */
+static uint16_t TXByteCtr = 0;
+
+/* Index for transmitting bytes. */
+static uint16_t TXByteIndex = 0;
+
+/* Number of iterations in the receiving interrupt. */
+static uint16_t TXIter = 0;
+
+int main(void)
+{
+    /* UART baud rate. */
+    constexpr unsigned int baudrate = 115200;
+
     DelfiPQcore::initMCU();
 
     delay_init();
 
-    MAP_UART_disableModule(EUSCI_A0_BASE);   //disable UART operation for configuration settings
-    //MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    /* Initialize UART baud rate. */
+    Console::init(baudrate);
 
-    const eUSCI_UART_ConfigV1 uartConfig = {
-        EUSCI_A_UART_CLOCKSOURCE_SMCLK,                 // SMCLK Clock Source
-        78,                                             // BRDIV = 78
-        2,                                              // UCxBRF = 2
-        0,                                              // UCxBRS = 0
-        EUSCI_A_UART_NO_PARITY,                         // No Parity
-        EUSCI_A_UART_LSB_FIRST,                         // LSB First
-        EUSCI_A_UART_ONE_STOP_BIT,                      // One stop bit
-        EUSCI_A_UART_MODE,                              // UART mode
-        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION   // Oversampling
-    };
+    delay_ms(1000);
 
+    /* Enabling interrupts for UART. */
+    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
+    MAP_Interrupt_enableMaster();
 
-    MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
-    MAP_UART_enableModule(EUSCI_A0_BASE);
+    MAP_UART_transmitData(EUSCI_A0_BASE, 'loop');
 
-    MAP_UART_transmitData(EUSCI_A0_BASE, 'test');
+    /* Busy loop. */
+    while (1u)
+    {
+        MAP_UART_transmitData(EUSCI_A0_BASE, 'testt');
 
-    while (1u) {
-        MAP_UART_transmitData(EUSCI_A0_BASE, 'loop');
         delay_ms(1000);
+    }
+}
+
+/**
+ * UART receive interrupt.
+ *
+ * Each message received starts with a two-byte length followed by that many
+ * image bytes.
+ */
+extern "C" void EUSCIA0_IRQHandler(void)
+{
+    uint_fast8_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
+
+    /* Clear interrupt flags so it can be triggered again. */
+    MAP_UART_clearInterruptFlag(EUSCI_A0_BASE, status);
+
+    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+    {
+        /* Get first length byte. */
+        if (TXIter == 0)
+        {
+            TXLength = MAP_UART_receiveData(EUSCI_A0_BASE);
+        }
+            /* Get second length byte. */
+        else if (TXIter == 1)
+        {
+            TXLength |= MAP_UART_receiveData(EUSCI_A0_BASE) << 8;
+        }
+            /* Get all image bytes. */
+        else
+        {
+            TXData[TXByteCtr++] = MAP_UART_receiveData(EUSCI_A0_BASE);
+        }
+
+        /* Initiate I2C transfer if all bytes have been received. */
+        if (TXIter > 1 && TXByteCtr == TXLength)
+        {
+            TXByteIndex = 1;
+            MAP_I2C_masterSendMultiByteStart(EUSCI_B0_BASE, TXData[0]);
+        }
+
+        TXIter++;
     }
 }
