fca10b65b29850e5bcfea3ed80bb318fccc34081
Dennisman219 <d.r.vangilst@live.nl>
2024-08-16 17:02:44 +0200

    removed double declaration of pcu_housekeeping_data_reply struct

--- src/EPS.h
+++ src/EPS.h
@@ -88,21 +88,6 @@ public:
         uint8_t error;
     };
 
-    struct pcu_housekeeping_data_reply {
-        uint8_t stid;
-        uint8_t ivid;
-        uint8_t rc;
-        uint8_t bid;
-        uint8_t stat;
-
-        uint16_t volt_brdsup;
-        uint16_t temp;
-        uint8_t vip_output[6];
-        uint8_t cc[4][14];      // Response CC_1 stored in cc[1]. Bytes are kept in the order that they are read
-
-        uint8_t error;
-    };
-
     static standard_reply reset_watchdog(DWire &wire, uint8_t i2c_address);
     static standard_reply no_operation(DWire &wire, uint8_t i2c_address);
     static standard_reply system_reset(DWire &wire, uint8_t i2c_address);
